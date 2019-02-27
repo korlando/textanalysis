97aa70234c773167cb71415dc6747a088d08f3e2
Keith Orlando <keith@airk.ai>
2019-02-27 01:54:30 -0500

    update auxiliary and pronouns

--- src/pkg/summary/pronouns.go
+++ src/pkg/summary/pronouns.go
@@ -1,6 +1,6 @@
 package summary
 
 var (
-	PersonalPronouns   = []string{"i", "me", "we", "us", "you", "u", "she", "her", "he", "him", "they", "them"}
-	ImpersonalPronouns = []string{"it", "it's", "that", "those", "this", "these"}
+	PersonalPronouns   = []string{"i", "me", "mine", "mine's", "my", "myself", "i'm", "im", "ikr", "i'd", "i've", "i'll", "we", "us", "our", "we're", "we'd", "we'll", "we've", "ourself", "ourselves", "you", "your", "yours", "yourself", "yourselves", "thou", "you're", "u", "you've", "you'll", "you'd", "she", "he", "her", "him", "hers", "his", "herself", "himself", "she'd", "he'd", "she's", "he's", "she'll", "he'll", "they", "they'll", "they've", "they're", "them", "themself", "themselves"}
+	ImpersonalPronouns = []string{"it", "it's", "it'll", "it'd", "that", "that's", "that'll", "that'd", "those", "this", "these"}
 )
